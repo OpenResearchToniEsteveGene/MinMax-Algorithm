605c09f743cb74f6874fa4cecd68722433c99990
Toni Esteve Gene et al. <toni2.estevegene@gmail.com>
2026-03-11 21:33:41 +0100

    Delete 4enratlla.h

--- 4enratlla.h
+++
@@ -1,5 +0,0 @@
-#define DIMENSIO 8
-
-void inicialitzartaulell(char taulell[DIMENSIO][DIMENSIO]);
-void mostraTauler(char taulell[DIMENSIO][DIMENSIO]);
-int taulerPle(char taulell[DIMENSIO][DIMENSIO]);
